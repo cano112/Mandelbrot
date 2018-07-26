700f014bc0c06b0b65d6e42a49ea72f9464252b0
Kamil Noster <knoster@student.agh.edu.pl>
2018-07-26 15:51:05 +0200

    Fix formatting

--- mandelbrot.asm
+++ mandelbrot.asm
@@ -1,14 +1,14 @@
 DATA segment
 		
 		ArgV						db 256 dup(?)				;args table
-		ArgC						db 0						;args number
-		ArgPtr      				dw 9 dup(?)					;pointers to args
+		ArgC						db 0					;args number
+		ArgPtr      					dw 9 dup(?)				;pointers to args
 		
 		Multiplier					dw 10d
 		CompValue					dq 4.0						
-		ScreenCoord                 dw ?						;N,M
+		ScreenCoord                 			dw ?					;N,M
 		
-		integerNumber				dw ?
+		integerNumber					dw ?
 		floatNumber					dq ?
 		
 		xMin						dq ?
@@ -17,10 +17,10 @@ DATA segment
 		yMax						dq ?
 		
 		ScreenWidth					dw 320d
-		ScreenHeight				dw 200d
+		ScreenHeight					dw 200d
 		
-		P							dq ?
-		Q							dq ?
+		P						dq ?
+		Q						dq ?
 									
 		ErrMsg1						db "No arguments!$"
 		ErrMsg2						db "Wrong arguments!$"
@@ -29,8 +29,8 @@ DATA segment
 DATA ends
 CODE segment
 	
-	.386										;popa/pusha and FPU flags to AX
-	.387										;FPU
+	.386									;popa/pusha and FPU flags to AX
+	.387									;FPU
 	
 	START:
 									
@@ -56,83 +56,83 @@ CODE segment
 	parseArguments:
 		pusha
 			
-		xor cx, cx								;same as "mov cx, 0"
-		mov cl, byte ptr es:[80h]				;load args length to cx (loop counter)
-		mov si, 82h								;set source pointer
+		xor cx, cx							;same as "mov cx, 0"
+		mov cl, byte ptr es:[80h]					;load args length to cx (loop counter)
+		mov si, 82h							;set source pointer
 		mov al, es:[si]							
 		mov di, offset ArgV						;set destination pointer
 		
-		cmp cl, 0								;check if ArgC>0
+		cmp cl, 0							;check if ArgC>0
 		jne parseArgumentsLoop
 		
-		mov dx, offset ErrMsg1					;print error message
+		mov dx, offset ErrMsg1						;print error message
 		call handleError						
 		
 		parseArgumentsLoop:						;load args to ArgV
 			mov al, byte ptr es:[si]			
 			
-			cmp al, 20h							;check if space
+			cmp al, 20h						;check if space
 			je whiteSpace						;if space, skip to white space removal
 			
-			cmp al, 9h							;check if tab
+			cmp al, 9h						;check if tab
 			je whiteSpace						;if tab, skip to white space removal
 			
-			cmp al, 0Dh							;check if CR (enter pressed)
+			cmp al, 0Dh						;check if CR (enter pressed)
 			je endOfArgs					
 			
-			mov byte ptr ds:[di], al			;if normal char => load to ArgV
+			mov byte ptr ds:[di], al				;if normal char => load to ArgV
 			inc si								
 			jmp parseArgumentsLoopCont
 			
 			endOfArgs:
-			mov byte ptr ds:[di], '$'			;replace CR with end of text
+			mov byte ptr ds:[di], '$'				;replace CR with end of text
 			inc si
 			jmp parseArgumentsLoopCont
 			
-			whiteSpace:							;if white space found
-			mov byte ptr ds:[di], 0				;load args separator
+			whiteSpace:						;if white space found
+			mov byte ptr ds:[di], 0					;load args separator
 			
 			whiteSpaceLoop:						;remove all white spaces left
 				inc si								
 				mov al, byte ptr es:[si]
-				cmp al, 20h						;check if space
+				cmp al, 20h					;check if space
 				je whiteSpaceLoop				;if space found => skip to next char
-				cmp al, 9h						;check if tab
+				cmp al, 9h					;check if tab
 				je whiteSpaceLoop				;if tab found => skip to next char
 			
-			parseArgumentsLoopCont:		    ;at this point, we do not have any white spaces left
+			parseArgumentsLoopCont:		    			;at this point, we do not have any white spaces left
 			inc di
 		
 		loop parseArgumentsLoop
 		
 		mov si, offset ArgV						;load args source pointer
-		mov di, offset ArgPtr					;load arg pointers table
+		mov di, offset ArgPtr						;load arg pointers table
 		mov bx, offset ArgC						;load arg number pointer	
 		
-		mov word ptr ds:[di], si				;load first arg pointer
+		mov word ptr ds:[di], si					;load first arg pointer
 		inc si
 		inc byte ptr ds:[bx]
 		add di, 2
 		
 		
 		separateArgsLoop:
-			mov al, byte ptr ds:[si]			;load char to al
+			mov al, byte ptr ds:[si]				;load char to al
 			
-			inc si								;point next char
-			cmp al, '$'							;if char == $
-			je parseArgumentsCont				;break					
-			cmp al, 0							;if char != NUL
-			jne separateArgsLoop				;continue
-												;else
-			mov word ptr ds:[di], si			;load arg pointer to ArgPtr
-			inc byte ptr ds:[bx]				;increment ArgC
+			inc si							;point next char
+			cmp al, '$'						;if char == $
+			je parseArgumentsCont					;break					
+			cmp al, 0						;if char != NUL
+			jne separateArgsLoop					;continue
+										;else
+			mov word ptr ds:[di], si				;load arg pointer to ArgPtr
+			inc byte ptr ds:[bx]					;increment ArgC
 		
-			add di, 2							;go to next arg pointer in ArgPtr (word=2byte)
+			add di, 2						;go to next arg pointer in ArgPtr (word=2byte)
 			jmp separateArgsLoop
 			
 		parseArgumentsCont:
 		
-												;fix first argument when there are more than one white spaces before first arg (DOSBox issue)
+										;fix first argument when there are more than one white spaces before first arg (DOSBox issue)
 		mov bx, 0
 		call getArgument
 		cmp byte ptr ds:[bx], 0
@@ -150,18 +150,18 @@ CODE segment
 		push bx
 		
 												
-		xor bx, bx								;getArgument uses whole bx
+		xor bx, bx							;getArgument uses whole bx
 		mov bl, al
 		call getArgument
-		xor ax, ax								;reset counter
+		xor ax, ax							;reset counter
 		
 		getArgumentLengthLoop:
 			inc al	
 			inc bx
-			cmp byte ptr ds:[bx], 0			;if end of argument => break
+			cmp byte ptr ds:[bx], 0					;if end of argument => break
 			je getArgumentLengthBreak
 		
-			cmp byte ptr ds:[bx], '$'		;if end of arguments => break
+			cmp byte ptr ds:[bx], '$'				;if end of arguments => break
 			je getArgumentLengthBreak
 			
 			jmp getArgumentLengthLoop
@@ -198,24 +198,24 @@ CODE segment
 		pusha
 		
 		xor bx, bx								
-		mov bl, al								;input for getArgument
+		mov bl, al							;input for getArgument
 		
-		call getArgumentLength					;input: al(arg index)   output: al (length)
+		call getArgumentLength						;input: al(arg index)   output: al (length)
 		cmp al, 3d
 		jb formatErrorMsg
 		
 		call getArgument						
-		mov si, bx								;si - argument pointer
-		xor cx, cx								;cl  - arg length    ch - current length
-		mov cl, al								;cl - argument length
-		xor ax, ax								;ax - place for binary number
-		xor dx, dx								;dl - current ASCII number
-		xor bx, bx								;bl - sign flag
+		mov si, bx							;si - argument pointer
+		xor cx, cx							;cl  - arg length    ch - current length
+		mov cl, al							;cl - argument length
+		xor ax, ax							;ax - place for binary number
+		xor dx, dx							;dl - current ASCII number
+		xor bx, bx							;bl - sign flag
 		
 		mov dl, byte ptr ds:[si]
-		cmp dl, '-'								;check sign
+		cmp dl, '-'							;check sign
 		jne toFloatLoop
-			mov bl, 1d							;1 => -     0 => +
+			mov bl, 1d						;1 => -     0 => +
 			inc ch
 			inc si
 		
@@ -235,16 +235,16 @@ CODE segment
 				mov di, ax
 				pop ax
 				
-				cmp ch, cl						;number must have fractional part
+				cmp ch, cl					;number must have fractional part
 				jnb formatErrorMsg
 		
-					cmp ch, 1d						;it means .1234
+					cmp ch, 1d				;it means .1234
 					je formatErrorMsg
 		
 						cmp bl, 1d
 						jne skipFormatCheck
 				
-							cmp ch, 2d						;it means: -.1234
+							cmp ch, 2d		;it means: -.1234
 							je formatErrorMsg
 				
 				skipFormatCheck:
@@ -260,7 +260,7 @@ CODE segment
 				jb formatErrorMsg
 			
 			push dx
-			mul word ptr ds:[Multiplier]		;move ax left, mul uses dx
+			mul word ptr ds:[Multiplier]				;move ax left, mul uses dx
 			pop dx
 			
 			sub dl, "0"
@@ -285,12 +285,12 @@ CODE segment
 	
 		cmp bl, 1d
 		jne skipSignChange
-			fchs								;change sign
+			fchs							;change sign
 		
 		skipSignChange:
-			fstp qword ptr ds:[floatNumber]		;move to mem
+			fstp qword ptr ds:[floatNumber]				;move to mem
 			
-			fstp st(0)							;remove from stack
+			fstp st(0)						;remove from stack
 		
 		jmp asciiToFloatEnd
 		
@@ -326,14 +326,14 @@ CODE segment
 		pop ax
 	ret
 	
-	getP:										;ax - x coord
-		mov word ptr ds:[ScreenCoord], ax		;cannot send ax directly to FPU
+	getP:									;ax - x coord
+		mov word ptr ds:[ScreenCoord], ax				;cannot send ax directly to FPU
 		
-		fild word ptr ds:[ScreenCoord]			;st(4)
-		fld qword ptr ds:[xMax]					;st(3)
-		fld qword ptr ds:[xMin]					;st(2)
-		fild word ptr ds:[ScreenWidth]			;st(1)
-		fldz									;st(0) - calculated p
+		fild word ptr ds:[ScreenCoord]					;st(4)
+		fld qword ptr ds:[xMax]						;st(3)
+		fld qword ptr ds:[xMin]						;st(2)
+		fild word ptr ds:[ScreenWidth]					;st(1)
+		fldz								;st(0) - calculated p
 		
 		fadd st(0), st(3)						;p = xMax
 		fsub st(0), st(2)						;p = xMax - xMin
@@ -342,20 +342,20 @@ CODE segment
 		fadd st(0), st(2)						;p = (xMax - xMin)*N/SCREEN_W + xMin
 		
 		fstp qword ptr ds:[P]					
-		fstp st(0)								;remove from stack
+		fstp st(0)							;remove from stack
 		fstp st(0)
 		fstp st(0)
 		fstp st(0)
 	ret
 	
-	getQ:										;ax - y coord
-		mov word ptr ds:[ScreenCoord], ax		;cannot send ax directly to FPU
+	getQ:									;ax - y coord
+		mov word ptr ds:[ScreenCoord], ax				;cannot send ax directly to FPU
 		
-		fild word ptr ds:[ScreenCoord]			;st(4)
-		fld qword ptr ds:[yMax]					;st(3)
-		fld qword ptr ds:[yMin]					;st(2)
-		fild word ptr ds:[ScreenHeight]			;st(1)
-		fldz									;st(0) - calculated p
+		fild word ptr ds:[ScreenCoord]					;st(4)
+		fld qword ptr ds:[yMax]						;st(3)
+		fld qword ptr ds:[yMin]						;st(2)
+		fild word ptr ds:[ScreenHeight]					;st(1)
+		fldz								;st(0) - calculated p
 		
 		fadd st(0), st(3)						;p = yMax
 		fsub st(0), st(2)						;p = yMax - yMin
@@ -364,73 +364,73 @@ CODE segment
 		fadd st(0), st(2)						;p = (yMax - yMin)*M/SCREEN_W + yMin
 		
 		fstp qword ptr ds:[Q]					
-		fstp st(0)								;remove from stack
+		fstp st(0)							;remove from stack
 		fstp st(0)
 		fstp st(0)
 		fstp st(0)
 	ret
 	
-	determinePixelColor:						;al = 1 || al = 0
+	determinePixelColor:							;al = 1 || al = 0
 		push cx
 		
-		fld qword ptr ds:[P]					;st(7) = p
-		fld qword ptr ds:[Q]					;st(6) = q
-		fldz									;st(5) = x
-		fldz									;st(4) = y
-		fldz									;st(3) = x*x
-		fldz									;st(2) = y*y
-		fldz									;st(1) = tmp
-		fldz									;st(0) - auxiliary variable
+		fld qword ptr ds:[P]						;st(7) = p
+		fld qword ptr ds:[Q]						;st(6) = q
+		fldz								;st(5) = x
+		fldz								;st(4) = y
+		fldz								;st(3) = x*x
+		fldz								;st(2) = y*y
+		fldz								;st(1) = tmp
+		fldz								;st(0) - auxiliary variable
 		
 		mov cx, 1000d
 		detPixelColLoop:
 		
 			;tmp (1st iteration: x*x == 0 && y*y == 0)
-			fsub st(0), st(0)						;av = 0
-			fadd st(0), st(3)						;av = x*x
-			fsub st(0), st(2)						;av = x*x - y*y
-			fadd st(0), st(7)						;av = x*x - y*y + p
-			fxch st(1)								;swap(av, tmp)
+			fsub st(0), st(0)					;av = 0
+			fadd st(0), st(3)					;av = x*x
+			fsub st(0), st(2)					;av = x*x - y*y
+			fadd st(0), st(7)					;av = x*x - y*y + p
+			fxch st(1)						;swap(av, tmp)
 			
 			
 			;y
-			fsub st(0), st(0)						;av = 0
-			fadd st(0), st(5)						;av = x
-			fmul st(0), st(4)						;av = x*y
-			fadd st(0), st(0)						;av = 2*x*y
-			fadd st(0), st(6)						;av = 2*x*y + q
-			fxch st(4)								;swap(av, y)
+			fsub st(0), st(0)					;av = 0
+			fadd st(0), st(5)					;av = x
+			fmul st(0), st(4)					;av = x*y
+			fadd st(0), st(0)					;av = 2*x*y
+			fadd st(0), st(6)					;av = 2*x*y + q
+			fxch st(4)						;swap(av, y)
 			
 			;x
-			fsub st(0), st(0)						;av = 0
-			fadd st(0), st(1)						;av = tmp
-			fxch st(5)								;swap(av, x)
+			fsub st(0), st(0)					;av = 0
+			fadd st(0), st(1)					;av = tmp
+			fxch st(5)						;swap(av, x)
 			
 			;x*x
-			fsub st(0), st(0)						;x*x = 0
-			fadd st(0), st(5)						;x*x = x
-			fmul st(0), st(0)						;x*x = x*x
-			fxch st(3)								;swap(av x*x)
+			fsub st(0), st(0)					;x*x = 0
+			fadd st(0), st(5)					;x*x = x
+			fmul st(0), st(0)					;x*x = x*x
+			fxch st(3)						;swap(av x*x)
 			
 			;y*y
-			fsub st(0), st(0)						;y*y = 0
-			fadd st(0), st(4)						;y*y = y
-			fmul st(0), st(0)						;y*y = y*y
-			fxch st(2)								;swap(av, y*y)
+			fsub st(0), st(0)					;y*y = 0
+			fadd st(0), st(4)					;y*y = y
+			fmul st(0), st(0)					;y*y = y*y
+			fxch st(2)						;swap(av, y*y)
 			
 			;x*x + y*y to auxiliary variable
-			fsub st(0), st(0)						;av = 0
-			fadd st(0), st(3)						;av = x*x
-			fadd st(0), st(2)						;av = x*x + y*y
+			fsub st(0), st(0)					;av = 0
+			fadd st(0), st(3)					;av = x*x
+			fadd st(0), st(2)					;av = x*x + y*y
 			
-			fcom qword ptr ds:[CompValue]			;compares st(0) with CompValue and sets FPU flags
-			fstsw ax								;copies FPU flag register to AX
-			sahf									;copies AH to CPU's flags register
+			fcom qword ptr ds:[CompValue]				;compares st(0) with CompValue and sets FPU flags
+			fstsw ax						;copies FPU flag register to AX
+			sahf							;copies AH to CPU's flags register
 			ja breakDetPixelColLoop
 			
 		loop detPixelColLoop
 		
-		mov al, 1d									;1000 iterations done correctly
+		mov al, 1d							;1000 iterations done correctly
 		jmp detPixelColEnd
 		
 		breakDetPixelColLoop:
@@ -454,12 +454,12 @@ CODE segment
 		push dx
 		push ax
 		
-		xor ah, ah								;switch to graphic mode
+		xor ah, ah							;switch to graphic mode
 		mov al, 13d
 		int 10h
 		
-		xor cx, cx								;columns (Y)
-		xor dx, dx								;rows (X)
+		xor cx, cx							;columns (Y)
+		xor dx, dx							;rows (X)
 		
 		loopY:
 			cmp dx, word ptr ds:[ScreenHeight]
@@ -510,7 +510,7 @@ CODE segment
 		pop cx
 	ret
 	
-	print:										;prints text from ds:dx, use "mov dx, offset <label>" before
+	print:									;prints text from ds:dx, use "mov dx, offset <label>" before
 		push ax
 		
 		mov ah, 9h
@@ -532,7 +532,7 @@ CODE segment
 CODE ends
 
 STACK1 segment STACK
-							dw 127 dup(?)
+					dw 127 dup(?)
 		StackPointer		dw ?
 
 STACK1 ends
